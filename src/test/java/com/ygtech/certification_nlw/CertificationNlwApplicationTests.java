4cfa0a3ea62db46568059362800c53e1fbf0a4e7
Yuri Geovani <yuri_geovani@hotmail.com>
2024-02-06 19:29:22 +0000

    first commit

---
+++ src/test/java/com/ygtech/certification_nlw/CertificationNlwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ygtech.certification_nlw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificationNlwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
